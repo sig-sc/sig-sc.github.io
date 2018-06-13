4680b54d361d2ba56f9f1cede62f592e3938033f
Masahide Nakamura <masa-n@cs.kobe-u.ac.jp>
2018-06-13 12:32:47 +0900

    Convert past seminar info to posts

---
+++ scripts/_posts/2016-06-03-report-of-seminar.md
@@ -0,0 +1,69 @@
+---
+layout: post
+title:  "サービスコンピューティング研究専門委員会 第18回研究会 2016年6月3日(金),4日(土)"
+date:   2016-06-03 21:00:00 +0900
+categories: seminar report update
+---
+
+###サービスコンピューティング研究専門委員会 第18回研究会
+- ___テーマ:___ 「クラウド・ビックデータ・知能コミュニケーションシステムを指向するサービスコンピューティング」および一般
+- ___日程:___ 2016年6月3日(金),4日(土)
+- ___場所:___ 会津大学UBIC3DTheater
+- ___プログラム: ___
+
+<pre>
+
+6月3日(金)
+13:00〜　オープニング
+
+13：05～14：45 ビックデータ及びストリーミングプロセス
+(1) 13:05 - 13:30
+Consideration of QoS-Awareness in Transaction Models for Streaming Processing.
+○Shinji Kikuchi・Subhash Bhalla（UoA）
+(2) 13:30 - 13:55
+Analyzing Resident Location using Infrared Sensor Array
+○Yuta Kobiyama・Qiangfu Zhao・Kazuki Omomo（u-aizu）
+(3) 13:55 - 14:20
+Optimal allocation algorithms for geo-distributed data center topology
+○Yuichi Yamada・Chen Wuhui・Yuji Ishiduka・Paik Incheon（Aizu Univ.）
+(4) 14:20 - 14:45
+再生可能エネルギー分野におけるビッグデータ技術に関するフレームワークの研究
+○山崎治郎（会津大）
+
+14：45～15：00 休憩
+
+15：00～16：40 サービス発見と応用
+(5) 15:00 - 15:25
+スマートホームにおける大規模ログを活用した宅内でんき予報サービス
+増田哲也・佐伯幸郎（神戸大）・まつ本真佑（阪大）・○中村匡秀（神戸大）
+(6) 15:25 - 15:50
+オントロジ―と機械学習によるウェブサービスクラスタリングの改善
+○Rupasingha Arachchilage Hiruni Madhusha Rupasingha・Incheon Paik（UoA）
+・Banage Thenne Gedara Samantha Kumara（SUSL）
+(7) 15:50 - 16:15
+Construction of Global Social Service Network for Discovery Based on Big Data Infrastructure
+○Yutaka Koshiba・Incheon Paik・Wuhui Chen（Aizu Univ.）
+(8) 16:15 - 16:40
+Low Cost Line-Following and Load-Picking Robot
+○Isuru Jayarathne（University of Aizu）・Senaka Amarakeerthi（SUSL）
+・Michael Cohen（University of Aizu）
+
+6月4日(土) 
+9：00～10：00 WEIE招待講演
+Interpretation of Deep Learning (Qiangfu Zhao, University of Aizu)
+
+10：00～10：50 サービスとオントロジー （）
+(10) 10:00 - 10:25
+Big Data Analytic Service Discovery using Social Service Network with Workflow Awareness
+○Akila Siriweera・Incheon Paik（University of Aizu）
+(11) 10:25 - 10:50
+Evaluation of a Web Page Readability Using Ontology Reasoning
+○Yui Takeda・Incheon Paik（UOA）
+
+10：50～11：05 休憩
+
+11：05～12：00 ポスターセッション
+［ポスター講演］Performance Evaluation of Taxonomy Classification Using Machine Learning
+Hikaru Sakai・○Rupasingha Arachchilage Hiruni Madhusha Rupasingha・Incheon Paik（UoA）
+</pre>
+
---
+++ scripts/parse_seminar_list.pl
@@ -0,0 +1,124 @@
+#!/usr/bin/perl -w
+
+use strict;
+use LWP::Simple;
+use HTML::TreeBuilder;
+use Encode;
+use Data::Dumper;
+
+#my $url = 'http://sig-sc.org/seminar_list.html';
+#my $html = get($url);
+
+# コマンドラインで与えられたローカルHTMLファイルを読み込む
+my $db = &parse_html_file($ARGV[0]);
+
+for my $date (sort {$b cmp $a} (keys %{$db})) {
+	my $s = $db->{$date};
+	&generate_post_file($s);
+}
+
+# 各回のデータから，Jekyllのpostファイルを生成する
+sub generate_post_file {
+	my $obj = shift;
+
+	my $post_dir = "_posts";
+	my $file_name = sprintf "%s-report-of-seminar.md", $obj->{dateStr};
+	
+	open (my $out, ">$post_dir/$file_name") || die;
+	
+	print $out "---\n";
+	print $out "layout: post\n";
+	print $out "title:  \"$obj->{heading} $obj->{date}\"\n";
+	print $out "date:   $obj->{dateStr} 21:00:00 +0900\n";
+	print $out "categories: seminar report update\n";
+	print $out "---\n\n";
+
+	print $out "###$obj->{heading}\n";
+	print $out "- ___テーマ:___ $obj->{theme}\n";
+	print $out "- ___日程:___ $obj->{date}\n";
+	print $out "- ___場所:___ $obj->{location}\n";
+	print $out "- ___プログラム: ___\n";
+	print $out "\n<pre>\n";
+	print $out "$obj->{content}";
+	print $out "</pre>\n\n";
+
+	close $out;
+}
+
+
+# 旧SC研のHTMLファイルをパースして，documentを作成
+sub parse_html_file {
+	my $html = shift;
+
+	#とりあえずファイルを行で保持
+	open (my $fh, $html) || die;
+	my @lines = <$fh>;
+	close $fh;
+	#全角：を半角に
+	@lines = map { $_ =~ s/：/:/g; $_; } @lines;
+
+	#DOMを生成する
+	open($fh, "<:utf8", $html) || die;
+	my $dom = HTML::TreeBuilder->new;
+	$dom->parse_file($fh);
+
+
+	## タイトルを取得
+	my @headings;
+	for my $h2 ($dom->find('h2')) {
+		my $str = Encode::encode('utf8', $h2->as_text);
+		if ($str !~ /MENU|著作権|過去のイベント/) {
+			push @headings, $str;
+		}
+	}
+
+	## コンテンツ(プログラム)を取得
+	my @contents;
+	for my $pre ($dom->find('pre')) {
+		my $str = Encode::encode('utf8', $pre->as_text);
+		push @contents, $str;
+	}
+
+	## 日程，ロケ，テーマを取得
+	my @themes = map {$_ =~ s/<.*?>//g; $_ =~ s/：/:/g; $_ =~ s/\s|　//g; $_ =~ /:(.*)/; $1;} 
+			grep { /テーマ:/ } @lines;
+	my @locations = map {$_ =~ s/<.*?>//g; $_ =~ s/：/:/g; $_ =~ s/\s|　//g; $_ =~ /:(.*)/; $1;} 
+					grep { /場所:/} @lines;
+	my @dates = map {$_ =~ s/\s|　//g; $_ =~ /(\d\d\d\d年.*)<br/;}
+					grep { /\d\d\d\d年/ } @lines;
+
+	## データオブジェクトを作る
+	my $db = {};
+	for (my $i=0; $i<@headings; $i++) {
+		
+		my $date = $dates[$i] || "未設定";
+		my $dateStr = $date;
+		if ($date =~ /(\d+)年(\d+)月(\d+)日/) {
+			$dateStr = sprintf "%04d-%02d-%02d", $1, $2, $3;
+		} else {
+			print STDERR "DATE ERROR: $date\n";
+		}
+		
+		my $location = $locations[$i] || "未設定";
+		my $theme = $themes[$i] || "未設定";
+		my $heading = $headings[$i];
+		my $content = $contents[$i];
+		
+		my $seminar = {};
+		$seminar = {
+			date => $date,
+			dateStr => $dateStr,
+			location => $location,
+			theme => $theme,
+			heading => $heading,
+			content => $content
+		};
+		$db->{$dateStr} = $seminar;
+	}
+
+
+	return $db;
+
+}
+
+
